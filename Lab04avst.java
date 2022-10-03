c9f60ac4d620a106066c36ebe14a63f3108e783c
Antonio Martin <antonio.martin@vtsd.com>
2022-10-03 12:12:48 -0400

    Lab04

--- Lab04avst.java
+++ Lab04avst.java
@@ -13,15 +13,33 @@ public class Lab04avst
         double annualRate = 4.85;
         double numYears   = 30;
 
+        double monthlyRate = (annualRate / 100) / 12;
+
+        double numMonths = numYears * 12;
+        double numerator = monthlyRate * Math.pow(1 + monthlyRate,numMonths);
+
+        System.out.println(numerator);
+        System.out.println(monthlyRate);
+
+        double denominator = Math.pow(1 + annualRate,numMonths);
+
+
+
+        // monthly payment
+        double temp = 3.141529;
+        System.out.println(temp);
+        temp *=100;
+        System.out.println(temp);
+        temp = Math.round(temp);
+        System.out.println(temp);
+        temp /= 100;
+        System.out.println(temp);
 
 
 
 
 
 
-
-
-        System.out.println();
     }
 }
 
